e99ab600bca725bfad68d8ec12ef23f47aaa95e7
Chioma Okeke <omaokeke28@gmail.com>
2020-11-28 08:53:12 +0100

    Remodified the page

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function openSlideMenu() {
-    document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('').style.marginLeft = '250';
-}
-
-function closeSlideMenu() {
-    document.getElementById('side-menu').style.width = '0';
-    document.getElementById('').style.marginLeft = '0';
-}
